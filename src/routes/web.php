fe0724607cdbf8fc9e7c7a77e7f46129a51f52ec
sfaragy <solimankhulna@gmail.com>
2025-06-16 13:11:36 +0600

    Web: Initiate route to guide users for API only in mind

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json(['message' => 'Welcome to Token2049 API. Ready to accept requests on /api']);
 });
